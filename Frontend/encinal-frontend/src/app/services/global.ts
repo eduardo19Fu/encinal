c3be6ae510d0879ccb3d1ad4b0d34346e8001adb
Eduardo Fuentes <eduardo.fuentes@viaro.net>
2022-10-12 13:57:38 -0600

    Se realizaron las configuraciones pertienentes para salir a produccion despues de aplicar el cambio de ingreso de fecha de venta manual en creación de nueva venta

--- Frontend/encinal-frontend/src/app/services/global.ts
+++ Frontend/encinal-frontend/src/app/services/global.ts
@@ -1,4 +1,4 @@
 export const global = {
-    url: 'http://localhost:8180/api'
-    // url: 'https://condadoelencinal.com:8180/api'
+    // url: 'http://localhost:8180/api'
+    url: 'https://condadoelencinal.com:8180/api'
 };
